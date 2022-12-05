51a028f5a56835ddeb87fbbab821bc13ab0868fd
LiangchengR <liangchengr@gmail.com>
2022-12-04 17:10:45 -0800

    day 1 pt 1 solution

--- day1/main.py
+++ day1/main.py
@@ -1,5 +1,22 @@
 def main():
-    print("Hello World!")
+    highest_calorie_count = 0
+    current_calorie_count = 0
+
+    with open("day1\day_1_input.txt") as f:
+        calories = f.read().splitlines()
+
+    for i in range(len(calories)):
+        if calories[i]:
+            current_calorie_count += int(calories[i])
+
+        else:
+            if current_calorie_count > highest_calorie_count:
+                highest_calorie_count = current_calorie_count
+                current_calorie_count = 0
+            else:
+                current_calorie_count = 0
+
+    return print(highest_calorie_count)
 
 
 if __name__ == "__main__":
